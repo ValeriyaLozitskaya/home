0e1232c839f72efc262a6a80d2302896e7f48b00
Lera <lozitskaya.lera@ukr.net>
2016-07-06 15:03:03 +0300

    js17-18

---
+++ js17-18/gulpfile.js
@@ -0,0 +1,134 @@
+'use strict';
+// Импортируем все наши плагины и сам gulp
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+// Создаем объект в который пропишем все нужные нам пути, чтобы при необходимости легко в одном месте их редактировать
+var path = {
+    build: { // Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { // Пути откуда брать исходники
+        html: 'src/*.html', // Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',// В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/css/main.scss',
+        img: 'src/img/**/*.*', // Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { // Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/css/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+//  Создадим переменную с настройками нашего dev сервера
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Puvvl"
+};
+// Таск для сборки html
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) // Выберем файлы по нужному пути
+        .pipe(rigger()) // Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) // Выплюнем их в папку build
+        .pipe(reload({stream: true})); // Перезагрузим наш сервер для обновлений
+});
+// Rigger это наш плагин, позволяющий использовать такую конструкцию для импорта файлов: //= template/footer.html
+// Команда для сборки - gulp html:build
+
+// Собираем JS, команда: gulp js:build
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) // Найдем наш main файл
+        .pipe(rigger()) // Прогоним через rigger
+        .pipe(sourcemaps.init()) // Инициализируем sourcemap
+        .pipe(uglify()) // Сожмем наш js
+        .pipe(sourcemaps.write()) // Пропишем карты
+        .pipe(gulp.dest(path.build.js)) // Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); // Перезагрузим сервер
+});
+// Собираем SCSS, команда: gulp style:build
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) // Выберем наш main.scss
+        .pipe(sourcemaps.init()) // Инициализируем sourcemap
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.write()) // Пропишем карты
+        .pipe(gulp.dest(path.build.css)) // Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); // Перезагрузим сервер
+});
+// Таск по картинкам, команда gulp image:build
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) // Выберем наши картинки
+        .pipe(imagemin({ // Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img)) // Выплюнем их в build
+        .pipe(reload({stream: true})); // Перезагрузим сервер
+});
+// Таск по шрифтам, команда gulp fonts:build
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts) // Выберем наши шрифты
+        .pipe(gulp.dest(path.build.fonts)) // Выплюнем их в build
+});
+// ВЫПОЛНИТЬ ВСЕ! Запустить: gulp build
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'image:build',
+    'fonts:build'
+]);
+// При изменении(либо добавлении) какого то файла автоматически запускать нужную задачу: gulp watch - не советую использовать команду в командной строке ;-)
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+// Создание локального веб-сервера: gulp webserver
+gulp.task('webserver', function () {
+    browserSync(config);
+}); // Запустим livereload сервер с настройками, которые определены в объекте config.
+// ОЧИСТКА НЕ НУЖНЫХ ФАЙЛОВ В ПАПКЕ build, команда: gulp clean (очистит всю папку)!
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+// Дефолтный таск, который будет запускать всю сборку, команда: gulp
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ js17-18/readme.md
@@ -0,0 +1,40 @@
+﻿# Урок 17-18
+
+﻿Плагины:
+
+1. [gulp-autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer) — автоматически добавляет вендорные префиксы к CSS свойствам
+2. [gulp-minify-css](https://www.npmjs.com/package/gulp-minify-css) — нужен для сжатия CSS кода
+3. [browser-sync](https://www.npmjs.com/package/browser-sync) — с помощью этого плагина мы можем легко развернуть локальный dev сервер с блэкджеком и livereload, а так же с его помощью мы сможем сделать тунель на наш localhost, что бы легко демонстрировать верстку заказчику
+4. [gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin) — для сжатия картинок
+5. [imagemin-pngquant](https://www.npmjs.com/package/imagemin-pngquant) — дополнения к предыдущему плагину, для работы с PNG
+6. [gulp-uglify](https://www.npmjs.com/package/gulp-uglify) — будет сжимать наш JS
+7. [gulp-sass](https://www.npmjs.com/package/gulp-sass) — для компиляции нашего SCSS кода
+8. [gulp-sourcemaps](https://www.npmjs.com/package/gulp-sourcemaps) — возьмем для генерации css sourscemaps, которые будут помогать нам при отладке кода
+9. [gulp-rigger](https://www.npmjs.com/package/gulp-rigger) — это просто киллер фича. Плагин позволяет импортировать один файл в другой простой конструкцией `//= footer.html`и эта строка при компиляции будет заменена на содержимое файла footer.html
+10. [gulp-watch](https://www.npmjs.com/package/gulp-watch) — Будет нужен для наблюдения за изменениями файлов. 
+11. [rimraf](https://www.npmjs.com/package/rimraf) — что-бы можно было удалить папку node_modules
+
+`Спасибо Frontend Devil за отличную подборку! :-)`
+
+Домашнее задание:
+
+1. Установить [node.js](https://nodejs.org/en/)
+2. Установить [grunt.js](http://gruntjs.com/)
+3. Настроить сборку проекта простого. Должны выполняться задачи по конкатенации и минификации ваших `js` и `css` файлов
+4. Обычно системную папку `node_modules` принято добавлять в [.gitignore](https://git-scm.com/book/ru/v1/%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D1%8B-Git-%D0%97%D0%B0%D0%BF%D0%B8%D1%81%D1%8C-%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B9-%D0%B2-%D1%80%D0%B5%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9#Игнорирование-файлов), поскольку там хранится очень много файлов, и держать их в репозитории плохая практика
+5. Бонусное задание: настроить аналогичную сборку, но на [gulp.js](http://gulpjs.com/). Он немного сложнее для первого понимания, но более современный и быстрый. Настоятельно рекомендую вернуться к `gulp.js` после того как разберетесь с `grunt.js`
+6. Для работы с командной строкой в Windows, вам может быть полезным использовать git bash
+
+
+Полезные материалы:
+
+1. [Стандартные команды ОС UNIX](https://ru.wikipedia.org/wiki/UNIX#.D0.A1.D1.82.D0.B0.D0.BD.D0.B4.D0.B0.D1.80.D1.82.D0.BD.D1.8B.D0.B5_.D0.BA.D0.BE.D0.BC.D0.B0.D0.BD.D0.B4.D1.8B_.D0.9E.D0.A1_UNIX)
+2. [КОМАНДНАЯ СТРОКА WINDOWS](http://www.allmbs.ru/cmd-01.html)
+3. [Несколько полезностей по работе с NPM](https://habrahabr.ru/post/206678/)
+4. [Grunt для тех, кто считает штуки вроде него странными и сложными](http://frontender.info/grunt-is-not-weird-and-hard/)
+5. [Grunt, инструмент для сборки javascript проектов](https://habrahabr.ru/post/148274/)
+6. [Самые нужные плагины для Grunt](https://habrahabr.ru/post/251157/)
+7. [Сборка проектов с помощью Gulp.js. Семинар в Яндексе](https://habrahabr.ru/company/yandex/blog/239993/)
+8. [Приятная сборка frontend проекта](https://habrahabr.ru/post/250569/)
+9. [Скринкаст по Gulp](http://learn.javascript.ru/screencast/gulp)
+10. [Скринкаст Node.JS. Основные возможности и средства создания веб-сервисов, включая внутренние особенности самого сервера Node.JS](https://learn.javascript.ru/screencast/nodejs) - для тех кто хочет глубже разобраться в `Node.JS`
